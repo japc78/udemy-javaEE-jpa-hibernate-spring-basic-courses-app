df86a3806356c13b86bbda1b60fdc9581ca3cdd7
japc <japc.grafico@gmail.com>
2021-08-31 23:28:40 +0200

    Add entity course

---
+++ src/main/java/com/japcdev/coursesapp/entities/Course.java
@@ -0,0 +1,71 @@
+package com.japcdev.coursesapp.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="courses")
+public class Course {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+	private String description;
+	private int duration;
+	private double price;
+	
+
+	public Course() {
+	}
+
+
+	public Course(int id, String description, int duration, double price) {
+		super();
+		this.id = id;
+		this.description = description;
+		this.duration = duration;
+		this.price = price;
+	}
+
+
+	public int getId() {
+		return id;
+	}
+
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+
+	public String getDescription() {
+		return description;
+	}
+
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+
+	public int getDuration() {
+		return duration;
+	}
+
+
+	public void setDuration(int duration) {
+		this.duration = duration;
+	}
+
+
+	public double getPrice() {
+		return price;
+	}
+
+
+	public void setPrice(double price) {
+		this.price = price;
+	}	
+}
