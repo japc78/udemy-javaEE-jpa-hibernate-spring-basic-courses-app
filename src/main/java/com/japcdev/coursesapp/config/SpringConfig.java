4dcbcb97bf005dc13556ff7739ce8ffc149a2a59
japc <japc.grafico@gmail.com>
2021-09-01 23:43:03 +0200

    Config spring

---
+++ src/main/java/com/japcdev/coursesapp/config/SpringConfig.java
@@ -0,0 +1,57 @@
+package com.japcdev.coursesapp.config;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.jdbc.datasource.lookup.JndiDataSourceLookup;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@EnableTransactionManagement
+@ComponentScan(basePackages = {"com.japcdev.coursesapp.repository", "com.japcdev.coursesapp.service"})
+@Configuration
+@PropertySource(value = {"classpath:jndi.properties"})
+public class SpringConfig {
+	@Value("${ref-jndi}")
+    private String refData;
+	
+	@Bean
+    public DataSource dataSource() {
+        JndiDataSourceLookup dsLookup = new JndiDataSourceLookup();
+        dsLookup.setResourceRef(true);
+        return dsLookup.getDataSource(refData);
+    }
+	
+	//adaptador de Hibernate
+	@Bean
+	public HibernateJpaVendorAdapter adapter() {
+		HibernateJpaVendorAdapter adp=new HibernateJpaVendorAdapter();
+		adp.setDatabasePlatform("org.hibernate.dialect.MySQLDialect");
+		return adp;
+	}
+	
+	//factoria EntityManager: Objeto para acceder a capa de persistencia con JPA
+	@Bean
+	public LocalContainerEntityManagerFactoryBean factory(DataSource dataSource, HibernateJpaVendorAdapter adapter) {
+		LocalContainerEntityManagerFactoryBean factory=new LocalContainerEntityManagerFactoryBean();
+		factory.setPersistenceUnitName("coursesPU");
+		factory.setDataSource(dataSource);
+		factory.setPackagesToScan("com.japcdev.coursesapp.model");
+		factory.setJpaVendorAdapter(adapter);
+		return factory;
+	}
+	
+	//gestor de transacción
+	@Bean
+	public JpaTransactionManager txManager(LocalContainerEntityManagerFactoryBean factory) {
+		JpaTransactionManager manager=new JpaTransactionManager();
+		manager.setEntityManagerFactory(factory.getObject());
+		return manager;
+	}
+}
